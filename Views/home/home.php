6f7faeb6feb70093341a7cc142470d035d399af8
Be Nho <nhob2014868@student.ctu.edu.vn>
2023-11-09 16:31:42 +0700

    home

--- Views/home/home.php
+++ Views/home/home.php
@@ -11,9 +11,7 @@ require_once("banner.php")
                 <div class="section-title text-center">
                     <div class="product-tab nav nav-tabs">
                         <ul>
-                            <li class="active"><a data-toggle="tab" href="#arrival">Điện thoại <span>/</span></a></li>
-                            <li><a data-toggle="tab" href="#popular">Ốp lưng <span>/</span></a></li>
-                            <li><a data-toggle="tab" href="#best">Phụ kiện</a></li>
+                            <li class="active"><a data-toggle="tab" href="#arrival">Sách bán chạy</span></a></li>
                         </ul>
                     </div>
                 </div>
@@ -147,46 +145,14 @@ require_once("banner.php")
     </div>
 </div>
 <!-- tab-products section end -->
-<!-- collection section start -->
-<section class="collection-area collection-area2 section-padding">
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-4">
-                <div class="single-colect banner collect-one">
-                    <a href="?act=detail&id=<?= $data_random['0']['MaSP'] ?>"><img src="public/<?= $data_random['0']['HinhAnh1'] ?>" alt="" /></a>
-                </div>
-            </div>
-            <div class="col-sm-4">
-                <div class="colect-text ">
-                    <h4><a href="#"><?= $data_random['0']['TenSP'] ?></a></h4>
-                    <h5>Giá: <?= number_format($data_random['0']['DonGia']) ?> VNĐ</h5>
-                    <a href="?act=detail&id=<?= $data_random['0']['MaSP'] ?>">Mua ngay <i class="mdi mdi-arrow-right"></i></a>
-                </div>
-                <div class="collect-img banner margin single-colect">
-                    <a href="#"><img src="public/<?= $data_random['0']['HinhAnh2'] ?>" alt="" /></a>
-                </div>
-            </div>
-            <div class="col-sm-4">
-                <div class="collect-img banner single-colect">
-                    <a href="?act=detail&id=<?= $data_random['1']['MaSP'] ?>"><img src="public/<?= $data_random['1']['HinhAnh1'] ?>" alt="" /></a>
-                </div>
-                <div class="colect-text ">
-                    <h4><a href="?act=detail&id=<?= $data_random['1']['MaSP'] ?>"><?= $data_random['1']['TenSP'] ?></a></h4>
-                    <h5>Giá: <?= number_format($data_random['1']['DonGia']) ?> VNĐ</h5>
-                    <a href="?act=detail&id=<?= $data_random['1']['MaSP'] ?>">Mua ngay <i class="mdi mdi-arrow-right"></i></a>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-<!-- collection section end -->
+
 <!-- featured-products section start -->
 <section class="single-products  products-two section-padding extra-padding-bottom">
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
                 <div class="section-title text-center">
-                    <h2>Sản phẩm mới nhất</h2>
+                    <h2>Sách mới nhất</h2>
                 </div>
             </div>
         </div>
