42cb97720d6e72fec3dfdd1636f3e231d0ebf3f3
Be Nho <nhob2014868@student.ctu.edu.vn>
2023-11-12 22:03:20 +0700

    logo

--- Admin/MVC/Views/admin/menu.php
+++ Admin/MVC/Views/admin/menu.php
@@ -6,7 +6,7 @@
   <div class="sidebar-brand-icon rotate-n-15">
     <i class="fas fa-laugh-wink"></i>
   </div>
-  <div class="sidebar-brand-text mx-3">Books<sup>Store</sup></div>
+  <div class="sidebar-brand-text mx-3">Book<sup>Store</sup></div>
 </a>
 
 <!-- Divider -->
